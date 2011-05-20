33600c4c969c13e03639be50ebc15534bff63375
Yaakov Selkowitz <yselkowitz@users.sourceforge.net>
2011-05-20 02:12:02 -0500

    Change libexecdir to /usr/sbin to match openssh

--- postinstall.sh
+++ postinstall.sh
@@ -1,1 +1,1 @@
-/usr/sbin/update-alternatives --install "/usr/sbin/ssh-askpass" "ssh-askpass" "/usr/lib/ssh/x11-ssh-askpass.exe" 10
+/usr/sbin/update-alternatives --install "/usr/sbin/ssh-askpass" "ssh-askpass" "/usr/sbin/x11-ssh-askpass.exe" 10
